9a86c6986bc98a704596b287c275086888fffc85
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-04-15 15:10:00 -0500

    maven starter pack

---
+++ src/main/java/com/emmazarate/app/App.java
@@ -0,0 +1,13 @@
+package com.emmazarate.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
