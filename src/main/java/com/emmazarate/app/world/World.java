73073b1dfae491db458df20b6244f848cd68229a
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-04-15 18:20:03 -0500

    finished World constructor starter

--- src/main/java/com/emmazarate/app/world/World.java
+++ src/main/java/com/emmazarate/app/world/World.java
@@ -8,7 +8,12 @@ class World {
     int[] room_grid;
     int grid_size = 0;
 
-    public World( int starting_room, HashMap<Integer, Object> rooms, int[] room_grid, int grid_size)
+    public World( int starting_room, HashMap<Integer, Object> rooms, int[] room_grid, int grid_size) {
+        this.starting_room = starting_room;
+        this.rooms = rooms;
+        this.room_grid = room_grid;
+        this.grid_size = grid_size;
+    }
 
 
 }
